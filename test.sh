70307716ba3b7eebc2d1028229a0508a41532797
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-06-22 11:28:39 +0700

    Initial commit

---
+++ test.sh
@@ -0,0 +1,17 @@
+rm -rf build/test/
+cd src/main/
+javac org/usefullibs/greeter/impl/HelloWorld.java \
+  -d ../../build/test/ \
+  && echo 'Compiled org.usefullibs.greeter.impl.HelloWorld class!'
+cd ../test
+javac org/usefullibs/greeter/impl/HelloWorldTest.java \
+  -cp ../../build/test/:../../libs/junit-4.11.jar \
+  -d ../../build/test/ \
+  && echo 'Compiled org.usefullibs.greeter.impl.HelloWorldTest class!'
+
+cd ../../build/test
+java -cp ../../libs/junit-4.11.jar:../../libs/hamcrest-all-1.3.jar:. \
+  org.junit.runner.JUnitCore \
+  org.usefullibs.greeter.impl.HelloWorldTest
+chmod +x test.sh
+./test.sh
